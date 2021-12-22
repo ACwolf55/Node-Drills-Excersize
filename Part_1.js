478694f83418266d832c8541524676949261b0e4
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2021-12-22 16:57:48 -0700

    1st

---
+++ Part_1.js
@@ -0,0 +1,43 @@
+// 1. Using Node's built-in REPL feature, 
+// have the REPL show all of the 
+// modules it has access to.
+
+//
+
+//2. Using the REPL feature, Look up the different methods
+// Node.js modules have:  fs, util, events, path and http    
+
+// 3. use *module.exports* to ___
+
+ 
+// 4. use *exports* to___
+
+
+// 5 use *require* to ____
+
+
+// 6. use *__filename* to ___
+
+
+// 7. use *__dirname* to ___
+
+
+// 8. use *fs* to ___
+
+
+// 9. use *util* to ___
+
+
+// 10. use events to ___
+
+
+// 11. use path to ____
+
+
+// 12. use *http* module to ___
+
+
+// 13. Make Promise function using async/await
+
+
+
---
+++ Part_2.js
@@ -0,0 +1,20 @@
+//1. install express and 
+//chalk, morgan, nvm, debug, express, nodemon?
+// then import 
+
+//2. set app variable to express invoked
+
+
+// 3. set middleware with the *use* keyword on *app*
+//  and *express.json* to parse all endpoint data from JSON
+
+// 4. *app.post endpoint*  - test in postman
+
+// 5. *app.get endpoint*  - test in postman
+
+// 6. *app.delete* endpoint  - test in postman
+
+// 7. *app.put* endpoint   - test in postman
+
+
+// 8. ?Debug with chrome?
